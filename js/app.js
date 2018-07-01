74c258a5b3083c5995ad3eedcd4395f99fb34b26
Hugo Cowan <hugocowan@hotmail.co.uk>
2018-07-01 18:05:50 +0100

    Added burger to navbar, changed navbar colour to pastel blue. Placed h2 titles inside greyboxes. Small changes to layout.

---
+++ js/app.js
@@ -0,0 +1,13 @@
+function toggleLinks(burger){ // eslint-disable-line no-unused-vars
+  const content = document.getElementById('nav-content');
+  const nav = document.getElementsByTagName('nav')[0];
+  if(content.className === 'nav-content'){
+    content.className += ' responsive';
+    nav.className += 'responsive';
+  } else{
+    content.className = 'nav-content';
+    nav.className = '';
+  }
+  console.log(burger);
+  burger.classList.toggle('change');
+}
