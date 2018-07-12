9ffe4a3b764caa7fdbd6c2788aeab43ea10b6c91
Hugo Cowan <hugocowan@hotmail.co.uk>
2018-07-12 17:14:20 +0100

    Added function to change navbar position based on scrolling

--- js/app.js
+++ js/app.js
@@ -1,10 +1,33 @@
-function toggleLinks(burger){ // eslint-disable-line no-unused-vars
-  const content = document.getElementById('nav-content');
-  if(content.className === 'nav-content'){
-    content.className += ' responsive';
-  } else{
-    content.className = 'nav-content';
+function wrapper() {
+  function toggleLinks(burger){ // eslint-disable-line no-unused-vars
+    const content = document.getElementById('nav-content');
+    if(content.className === 'nav-content'){
+      content.className += ' responsive';
+    } else{
+      content.className = 'nav-content';
+    }
+    // console.log(burger);
+    burger.classList.toggle('change');
   }
-  // console.log(burger);
-  burger.classList.toggle('change');
+
+  // let oldPosition;
+  window.addEventListener('scroll', () => {
+
+    const navbar = document.getElementsByTagName('nav')[0];
+    const newPosition = window.scrollY;
+
+    console.log(window.scrollY);
+    if (newPosition <= 0) {
+      console.log('new position <= old one.');
+      // oldPosition = newPosition;
+      navbar.style.position = 'absolute';
+    } else {
+      console.log('old position > new one.');
+      // oldPosition = newPosition;
+      navbar.style.position = 'fixed';
+    }
+
+  });
 }
+
+wrapper();
