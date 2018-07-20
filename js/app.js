5b174bcc377375c6dd1ac0ef8f033a9abbd739c0
Hugo Cowan <hugocowan@hotmail.co.uk>
2018-07-20 09:24:46 +0100

    Removed function changing navbar from absolute to fixed. Stopped using http sourced image.

--- js/app.js
+++ js/app.js
@@ -11,26 +11,26 @@ function wrapper() {
   }
 
   // let oldPosition;
-  window.addEventListener('scroll', () => {
-
-    const navbar = document.getElementsByTagName('nav')[0];
-    const newPosition = window.scrollY;
-
-    console.log(window.scrollY);
-    if (newPosition <= 0) {
-      console.log('new position <= old one.');
-      // oldPosition = newPosition;
-      navbar.style.cssText =
-      'position: absolute; color: black';
-
-    } else {
-      console.log('old position > new one.');
-      // oldPosition = newPosition;
-      navbar.style.cssText =
-      'position: fixed; color: black';
-    }
-
-  });
+  // window.addEventListener('scroll', () => {
+  //
+  //   const navbar = document.getElementsByTagName('nav')[0];
+  //   const newPosition = window.scrollY;
+  //
+  //   console.log(window.scrollY);
+  //   if (newPosition <= 0) {
+  //     console.log('new position <= old one.');
+  //     // oldPosition = newPosition;
+  //     navbar.style.cssText =
+  //     'position: absolute; color: black';
+  //
+  //   } else {
+  //     console.log('old position > new one.');
+  //     // oldPosition = newPosition;
+  //     navbar.style.cssText =
+  //     'position: fixed; color: black';
+  //   }
+  //
+  // });
 }
 
 wrapper();
