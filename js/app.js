48fcf9ff1c89948fb98a20476bf9d7742be627ce
Hugo Cowan <hugocowan@hotmail.co.uk>
2018-07-22 06:35:13 +0100

    fixed burger not opening. Clicking the burger no longer takes you to the top of the page.

--- js/app.js
+++ js/app.js
@@ -1,36 +1,34 @@
-function wrapper() {
-  function toggleLinks(burger){ // eslint-disable-line no-unused-vars
-    const content = document.getElementById('nav-content');
-    if(content.className === 'nav-content'){
-      content.className += ' responsive';
-    } else{
-      content.className = 'nav-content';
-    }
-    // console.log(burger);
-    burger.classList.toggle('change');
-  }
 
-  // let oldPosition;
-  // window.addEventListener('scroll', () => {
-  //
-  //   const navbar = document.getElementsByTagName('nav')[0];
-  //   const newPosition = window.scrollY;
-  //
-  //   console.log(window.scrollY);
-  //   if (newPosition <= 0) {
-  //     console.log('new position <= old one.');
-  //     // oldPosition = newPosition;
-  //     navbar.style.cssText =
-  //     'position: absolute; color: black';
-  //
-  //   } else {
-  //     console.log('old position > new one.');
-  //     // oldPosition = newPosition;
-  //     navbar.style.cssText =
-  //     'position: fixed; color: black';
-  //   }
-  //
-  // });
+function toggleLinks(burger){ // eslint-disable-line no-unused-vars
+  // console.log(burger.target.value);
+  const content = document.getElementById('nav-content');
+  if(content.className === 'nav-content'){
+    content.className += ' responsive';
+  } else{
+    content.className = 'nav-content';
+  }
+  // console.log(burger);
+  burger.classList.toggle('change');
 }
 
-wrapper();
+// let oldPosition;
+// window.addEventListener('scroll', () => {
+//
+//   const navbar = document.getElementsByTagName('nav')[0];
+//   const newPosition = window.scrollY;
+//
+//   console.log(window.scrollY);
+//   if (newPosition <= 0) {
+//     console.log('new position <= old one.');
+//     // oldPosition = newPosition;
+//     navbar.style.cssText =
+//     'position: absolute; color: black';
+//
+//   } else {
+//     console.log('old position > new one.');
+//     // oldPosition = newPosition;
+//     navbar.style.cssText =
+//     'position: fixed; color: black';
+//   }
+//
+// });
